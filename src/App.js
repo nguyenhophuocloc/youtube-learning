a4940955b16677702b9cc87200566acc35c63d34
rongqui <rongmaphunkhoi@gmail.com>
2022-09-24 12:18:22 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { BasicTable } from "./components/BasicTable";
+import { FilteringTable } from "./components/FilteringTable";
+import { PaginationTable } from "./components/PaginationTable";
+import { SortingTable } from "./components/SortingTable";
+
+function App() {
+  return (
+    <div className="App">
+      <PaginationTable />
+    </div>
+  );
+}
+
+export default App;
